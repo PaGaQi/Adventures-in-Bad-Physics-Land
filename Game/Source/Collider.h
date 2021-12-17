3ed1aeae64a879b7b50746ed37cfad7a6dea5e7e
Fernando Almendro Martí <fernandoalmendromarti@gmail.com>
2021-12-17 19:11:23 +0100

    collider

---
+++ Game/Source/Collider.h
@@ -0,0 +1,40 @@
+#ifndef __COLLIDER_H__
+#define __COLLIDER_H__
+
+#include "SDL/include/SDL_Rect.h"
+#include "Point.h"
+#define MAX_LISTENERS 5
+
+class Module;
+
+struct Collider
+{
+	enum Type
+	{
+		NONE = -1,
+		WALL,
+		BULLET,
+		PLAYER,
+		ENEMY
+	};
+
+	//Methods
+	Collider(SDL_Rect rectangle, Type type, Module* listener = nullptr);
+
+	void SetPos(int x, int y);
+
+	iPoint GetPos() { return iPoint{ rect.x,rect.y }; }
+
+	bool Intersects(const SDL_Rect& r) const;
+
+	void AddListener(Module* listener);
+
+	//Variables
+	SDL_Rect rect;
+	bool pendingToDelete = false;
+	Type type;
+	Module* listeners[MAX_LISTENERS] = { nullptr };
+};
+
+
+#endif // !__COLLIDER_H__
